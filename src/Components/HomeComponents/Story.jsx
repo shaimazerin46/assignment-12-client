c1026a9ab6916ad3fdb1d9b5d016e9572112a10c
unknown <shaimazerin46@gmail.com>
2025-03-15 01:45:29 +0600

    story section created

---
+++ src/Components/HomeComponents/Story.jsx
@@ -0,0 +1,22 @@
+import story from '../../assets/images/story.jpg';
+
+
+const Story = () => {
+    return (
+        <div className="flex md:py-20 py-5 max-w-7xl mx-auto items-center">
+            <div className='mx-auto'>
+                <img src={story} alt="" className='h-[600px] w-[450px] object-cover rounded-[300px]'/>
+            </div>
+            <div className='w-[500px]'>
+                <h3 className='logo-font text-7xl font-light py-7'>Our story</h3>
+                <p className='text-justify'>
+                Welcome to HostelMeal — where every meal is more than just food; it’s an experience crafted for your taste buds and lifestyle. Whether you’re a busy professional needing quick, healthy options, a foodie seeking diverse cuisines, or a family planning weekly dinners, Feastify brings convenience and flavor to your doorstep. With personalized meal plans, fresh ingredients, and chef-curated recipes, you’ll spend less time cooking and more time savoring. Say goodbye to grocery lists and meal stress — and hello to delicious, hassle-free dining tailored just for you. Feastify: Fuel your day, one bite at a time.
+                </p>
+                
+            </div>
+           
+        </div>
+    );
+};
+
+export default Story;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import AllMeals from "../Components/HomeComponents/AllMeals";
 import Banner from "../Components/HomeComponents/Banner";
 import MealPrepareVideo from "../Components/HomeComponents/MealPrepareVideo";
 import Membarship from "../Components/HomeComponents/Membarship";
+import Story from "../Components/HomeComponents/Story";
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
         <div>
            
             <Banner></Banner>
+            <Story></Story>
             <AllMeals></AllMeals>
             <Membarship></Membarship>
             <MealPrepareVideo></MealPrepareVideo>
